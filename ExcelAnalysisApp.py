7fd34b6578310c33600649214c3a856cefd05651
Mostafa Gabr <124693861+mgabr2007@users.noreply.github.com>
2023-03-04 18:41:24 +0100

    Create ExcelAnalysisApp.py
    This is a Streamlit app for analyzing data in Excel files. The app allows the user to upload an Excel file, select columns to include, and view basic statistics about the data such as mean, maximum, and minimum values. The app also checks if the openpyxl library is installed and installs it if not.

---
+++ ExcelAnalysisApp.py
@@ -0,0 +1,59 @@
+import streamlit as st
+import pandas as pd
+
+# Install openpyxl if not already installed
+st.info("Checking if openpyxl is installed...")
+try:
+    import openpyxl
+except ImportError:
+    st.info("openpyxl not found, installing...")
+    !pip install openpyxl
+
+# Set page title
+st.set_page_config(page_title="Excel Analysis App")
+
+# Set page header
+st.header("Excel Analysis App")
+
+# Create file uploader
+uploaded_file = st.file_uploader("Upload an Excel file", type=["xlsx", "xls"])
+
+# If file is uploaded
+if uploaded_file is not None:
+    # Load Excel file into a pandas dataframe
+    try:
+        df = pd.read_excel(uploaded_file, engine="openpyxl")
+    except Exception as e:
+        st.error(f"Error: {str(e)}")
+        st.stop()
+
+    # Show dataframe
+    st.write("Dataframe:")
+    st.write(df.head())
+
+    # Select columns to include
+    st.write("Select columns to include:")
+    columns = list(df.columns)
+    selected_columns = []
+    for col in columns:
+        if st.checkbox(col, value=True):
+            selected_columns.append(col)
+    selected_df = df[selected_columns]
+    st.write("Selected dataframe:")
+    st.write(selected_df.head())
+
+    # Get some basic statistics about the data
+    st.write("Basic statistics about the data:")
+    st.write(selected_df.describe())
+
+    # Get the mean value for each column
+    st.write("Mean value for each column:")
+    st.write(selected_df.mean(numeric_only=True))
+
+    # Find the maximum value in the DataFrame
+    st.write("Maximum value for each column:")
+    st.write(selected_df.max(numeric_only=True))
+
+    # Find the minimum value in the DataFrame
+    st.write("Minimum value for each column:")
+    st.write(selected_df.min(numeric_only=True))
