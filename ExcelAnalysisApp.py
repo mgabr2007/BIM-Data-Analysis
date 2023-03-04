2446c2ea314deb72aa86a6817da35f6908b13ac5
Mostafa Gabr <124693861+mgabr2007@users.noreply.github.com>
2023-03-04 19:18:49 +0100

    Update ExcelAnalysisApp.py

--- ExcelAnalysisApp.py
+++ ExcelAnalysisApp.py
@@ -2,51 +2,59 @@ import streamlit as st
 import pandas as pd
 import subprocess
 
-# Install xlrd if not already installed
-with st.spinner('Installing xlrd...'):
-    subprocess.check_call(["pip", "install", "xlrd"])
+# Install openpyxl if not already installed
+st.info("Checking if openpyxl is installed...")
+try:
+    import openpyxl
+except ImportError:
+    st.info("openpyxl not found, installing...")
+    subprocess.call(["pip", "install", "openpyxl"])
 
-# Set page title
-st.set_page_config(page_title="Excel Analysis App")
+# Set page title and icon
+st.set_page_config(page_title="Excel Analysis App", page_icon=":bar_chart:")
 
 # Set page header
 st.header("Excel Analysis App")
 
-# Create file uploader in sidebar
-with st.sidebar:
-    st.write("## Upload a file")
-    uploaded_file = st.file_uploader("", type=["xlsx", "xls"])
+# Create file uploader
+uploaded_file = st.file_uploader("Upload an Excel file", type=["xlsx", "xls"])
 
 # If file is uploaded
 if uploaded_file is not None:
-    # Check file format
-    if uploaded_file.name.endswith('.xlsx') or uploaded_file.name.endswith('.xls'):
-        # Load Excel file into a pandas dataframe using xlrd engine
-        try:
-            df = pd.read_excel(uploaded_file, engine="xlrd")
-        except Exception as e:
-            st.error(f"Error: {str(e)}")
-            st.stop()
+    # Load Excel file into a pandas dataframe
+    try:
+        df = pd.read_excel(uploaded_file, engine="openpyxl")
+    except Exception as e:
+        st.error(f"Error: {str(e)}")
+        st.stop()
 
-        # Select columns to include in sidebar
-        st.sidebar.write("## Select columns to include")
-        columns = list(df.columns)
-        selected_columns = []
-        for col in columns:
-            if st.sidebar.checkbox(col, value=True):
-                selected_columns.append(col)
+    # Show dataframe
+    st.write("Dataframe:")
+    st.write(df.head())
 
-        # Show selected dataframe in main area
-        selected_df = df[selected_columns]
-        st.write("## Selected dataframe")
-        st.write(selected_df.head())
+    # Select columns to include
+    st.write("Select columns to include:")
+    columns = list(df.columns)
+    selected_columns = []
+    for col in columns:
+        if st.checkbox(col, value=True):
+            selected_columns.append(col)
+    selected_df = df[selected_columns]
+    st.write("Selected dataframe:")
+    st.write(selected_df.head())
 
-        # Show basic statistics
-        st.write("## Basic statistics")
-        st.write(selected_df.describe())
+    # Get some basic statistics about the data
+    st.write("Basic statistics about the data:")
+    st.write(selected_df.describe())
 
-        # Show mean, max, and min values for each column
-        st.write("## Summary statistics")
-        st.write(selected_df.agg(['mean', 'max', 'min']))
-    else:
-        st.error("Error: Invalid file format. Please upload an Excel file (.xlsx or .xls).")
+    # Get the mean value for each column
+    st.write("Mean value for each column:")
+    st.write(selected_df.mean(numeric_only=True))
+
+    # Find the maximum value in the DataFrame
+    st.write("Maximum value for each column:")
+    st.write(selected_df.max(numeric_only=True))
+
+    # Find the minimum value in the DataFrame
+    st.write("Minimum value for each column:")
+    st.write(selected_df.min(numeric_only=True))
